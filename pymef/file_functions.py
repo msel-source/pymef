e334cb4b58ee8bffabb6c731f97731b8ef4f8dc1
Jan Cimbalnik <jan.cimbalnik@gmail.com>
2017-05-26 14:35:33 +0200

    Some cosmetics + new function for reading uutc maps

--- pymef/file_functions.py
+++ pymef/file_functions.py
@@ -17,26 +17,27 @@ Mayo Clinic
 Rochester, MN
 United States
 """
+import os
 
 from .mef_file import pymef3_file
 import numpy as np
 
-def uutc_for_sample(sample,channel_info):
+def uutc_for_sample(sample,channel_md):
     
     # Sampling freq
-    fs = channel_info['section_2']['sampling_frequency']
+    fs = channel_md['section_2']['sampling_frequency']
     
-    seg_list = list(channel_info['segments'])
+    seg_list = list(channel_md['segments'])
     seg_list.sort()
     
     prev_sample = 0
-    prev_time = channel_info['segments'][seg_list[0]]['indices'][0]['start_time']
+    prev_time = channel_md['segments'][seg_list[0]]['indices'][0]['start_time']
     
     for segment in seg_list: 
-        indices = channel_info['segments'][segment]['indices']
+        indices = channel_md['segments'][segment]['indices']
         for index in indices:
             index_start_time = index['start_time']
-            index_start_sample = index['start_sample']  #TODO: !!!!Start sample might be 0 for each segment!!!! Check this
+            index_start_sample = index['start_sample']
             
             if index_start_sample > sample:
                 return int(prev_time + np.ceil(((sample - prev_sample) / fs)))
@@ -69,7 +70,7 @@ def sample_for_uutc(uutc,channel_md,return_discont_distance=False):
         indices = channel_md['segments'][segment]['indices']
         for index in indices:
             index_start_time = index['start_time']
-            index_start_sample = index['start_sample']   #TODO: !!!!Start sample might be 0 for each segment!!!! Check this
+            index_start_sample = index['start_sample']
             index_N_samples = index['number_of_samples']
             
             if index_start_time > uutc:
@@ -102,10 +103,6 @@ def uutc_check(uUTC,channel_md):
     chan_uutc_start = channel_md['channel_specific_metadata']['earliest_start_time']
     # Get the uUTC stop
     chan_uutc_stop = channel_md['channel_specific_metadata']['latest_end_time']
-    
-    if type(channel_md['section_3']['recording_time_offset']) == int:
-        chan_uutc_start = -chan_uutc_start + channel_md['section_3']['recording_time_offset']
-        chan_uutc_stop = -chan_uutc_stop + channel_md['section_3']['recording_time_offset']
         
     if (uUTC>=chan_uutc_start and uUTC<=chan_uutc_stop):
         return True
@@ -125,68 +122,63 @@ def get_discontinuities(channel_md):
     discont_index_list - list of lists discontinuity start indices for individual segments
     discont_uutc_list - list of lists discontinuity start uutc times for individual segments
     discont_sample_list - list of lists discontinuity start samples for individual segments
+    discont_sample_span_list - list of lists discontinuity span in samples for individual segments
     """
     
     discont_index_list = []
     discont_uutc_list = []
     discont_sample_list = []
+    discont_sample_span_list = []
     
     # Sampling freq
     fs = channel_md['section_2']['sampling_frequency']
-    
-    # Find out if offset is used
-    if type(channel_md['section_3']['recording_time_offset']) == int:
-        rt_offset = channel_md['section_3']['recording_time_offset']
-        offset_flag = True
-    else:
-        rt_offset = 0
-        offset_flag = False
-    
+        
     seg_list = list(channel_md['segments'])
     seg_list.sort()
     
     prev_N_samples = channel_md['segments'][seg_list[0]]['indices'][0]['number_of_samples']
     prev_time = channel_md['segments'][seg_list[0]]['indices'][0]['start_time']
-    if offset_flag:
-        prev_time = -prev_time + rt_offset
+    prev_block_span = (prev_N_samples / fs) * 1e6
+    prev_block_span += (1/fs) * 1e6
     
     for si,segment in enumerate(seg_list): 
         indices = channel_md['segments'][segment]['indices']
         
         # First block in segment is a discontinuity by definition
-        seg_discont_index_list = []
-        seg_discont_index_list.append(0)
-        seg_discont_uutc_list = []
-        seg_discont_uutc_list.append(indices[0]['start_time'])
-        seg_discont_sample_list = []
-        seg_discont_sample_list.append(indices[0]['start_sample']) #TODO: !!!!Start sample might be 0 for each segment!!!! Check this
+        seg_discont_index_list = [0]
+        seg_discont_uutc_list = [indices[0]['start_time']]
+        seg_discont_sample_list = [indices[0]['start_sample']]
+        if si == 0:
+            seg_discont_sample_span_list = [0]
+        else:
+            seg_discont_sample_span_list = [int(((indices[0]['start_time'] - (prev_time + prev_block_span)) / 1e6) * fs)]
         
-        for ii,index in enumerate(indices):
-            index_start_time = index['start_time']
-            if offset_flag:
-                index_start_time = -index_start_time + rt_offset
-            index_N_samples = index['number_of_samples']
-            
-            # Previous block time length
-            prev_block_span = prev_N_samples * fs * 1e6
-            
-            # Check for discontinuity
-            if index_start_time > prev_time+prev_block_span:
-                seg_discont_index_list.append(ii)
-                seg_discont_uutc_list.append(index['start_time'])
-                seg_discont_sample_list.append(index['start_sample']) #TODO: !!!!Start sample might be 0 for each segment!!!! Check this
+        if len(indices) > 1:
+
+            for ii,index in enumerate(indices):
+                index_start_time = index['start_time']
+                index_N_samples = index['number_of_samples']
                 
-            prev_time = index_start_time
-            prev_N_samples = index_N_samples
+                # Check for discontinuity
+                if (index_start_time - 1/fs * 1e6 > prev_time+prev_block_span) and ii > 0:
+                    seg_discont_index_list.append(ii)
+                    seg_discont_uutc_list.append(index_start_time)
+                    seg_discont_sample_list.append(index['start_sample'])
+                    seg_discont_sample_span_list.append(int(((index_start_time - (prev_time + prev_block_span)) / 1e6 ) * fs))
+                                        
+                prev_time = index_start_time
+                prev_N_samples = index_N_samples
+                prev_block_span = (prev_N_samples / fs) * 1e6
             
         discont_index_list.append(seg_discont_index_list)
         discont_uutc_list.append(seg_discont_uutc_list)
         discont_sample_list.append(seg_discont_sample_list)
+        discont_sample_span_list.append(seg_discont_sample_span_list)
         
-    return discont_index_list,discont_uutc_list,discont_sample_list
+    return discont_index_list,discont_uutc_list,discont_sample_list,discont_sample_span_list
 
-def read_ts_data_sample(session_path,
-                        password,channel_map,sample_map):
+def read_ts_channels_data_sample(session_path,
+                                 password,channel_map,sample_map):
     """
     Reads desired channels in desired sample segment
     
@@ -222,97 +214,104 @@ def read_ts_data_sample(session_path,
         
     return data_list
 
-#def read_ts_data_uutc(session_md,session_path,
-#                      password,channel_map,uutc_map,
-#                      on_out_of_channel_fail = True):
-#    """
-#    Reads desired channels in desired time segment. Missing data at
-#    discontinuities are filled with NaNs.
-#    
-#    Parameters:
-#    -----------
-#    session_md - mef3 session metadata dictionary\n
-#    session_path - path to mef3 session (.mefd)\n
-#    password - mef3 data password\n
-#    channel_map - list of channels to be read\n
-#    uutc_map - list of [start,stop] uutc times to be loaded that correspond\n
-#        to channel_map. if there is only one entry the same range is applied\n
-#        to all channels\n
-#    on_out_of_channel_fail - fail if uUTC times are out of channel times\n
-#        default = True
-#    
-#    Returns:
-#    --------
-#    data - numpy array [channels,samples]\n
-#    """
-#    
-#    data_list = []
-#    
-#    if len(uutc_map) == 1:
-#        uutc_map = uutc_map*len(channel_map)
-#        
-#    if len(uutc_map) != len(uutc_map):
-#        print('Length of uutc map is not equivalet to the length of channel map')
-#        return
-#    
-#    # Checkt that uUTCs are in file
-#    for channel,uutc_ss in zip(channel_map,uutc_map):
-#        channel_path = session_path+'/'+channel+'.timd'
-#        channel_md = session_md['time_series_channels'][channel]
-#        
-#        fs = channel_md['section_2']['sampling_frequency']
-#        
-#        # Get the size of the output array
-#        N_samples_out = int(np.ceil(((uutc_ss[1] - uutc_ss[0]) / 1e6) * fs))
-#        out_array = np.zeros(N_samples_out)
-#        out_array[:] = np.nan
-#        
-#        # Get discontinuities
-#        disc_uutc_list,disc_sample_list = get_discontinuities(channel_md)[1:]
-#        
-#        flat_uutcs = [i for sublist in disc_uutc_list for i in sublist]
-#        flat_samples = [i for sublist in disc_sample_list for i in sublist]
-#        
-#        # Find out if we hit any discontinuities, if so - get them
-#        disc_in_request = [x for x in zip(flat_uutcs,flat_samples) if (x[0] > uutc_ss[0] and x[0] < uutc_ss[1])]
-#        
-#        # Get the segment samples in mef and corresponding indices in output array
-#        if len(disc_uuts_in_request):
-#            out_array_idxs = []
-#            mef_idxs = []
-#            
-#            # Get the first mef sample and check if we hit discontinuity
-#            sample, disc_dist = sample_for_uutc(uutc_ss[0],channel_md,True)
-#            
-#            if disc_dist: #uutc was in discontinuity
-#                array_idx = disc_dist
-#            
-#            disc_sample = disc_in_request[0][1]
-#            
-#            for disc_uutc in disc_uuts_in_request:
-#                
-#                
-#        else:
-#            out_array_idxs = [[0,N_samples_out]]
-#            start_samp = sample_for_uutc(uutc_ss[0],channel_md)
-#            stop_samp = sample_for_uutc(uutc_ss[1],channel_md)
-#            mef_idxs[[start_samp,stop_samp]]
-#        
-#        
-#       
-#        
-#        
-#        
-#        
-#    return
+def read_ts_data_channel_uutc(session_md,session_path,
+                              password,channel_map,uutc_map):
+    """
+    Reads desired channels in desired time segment. Missing data at
+    discontinuities are filled with NaNs.
+   
+    Parameters:
+    -----------
+    session_md - mef3 session metadata dictionary\n
+    session_path - path to mef3 session (.mefd)\n
+    password - mef3 data password\n
+    channel_map - list of channels to be read\n
+    uutc_map - list of [start,stop] uutc times to be loaded that correspond\n
+       to channel_map. if there is only one entry the same range is applied\n
+       to all channels\n
+   
+    Returns:
+    --------
+    data - numpy array [channels,samples]\n
+    """
+   
+    data_list = [] # Creating a list since sampling frequency is not garanteed to be the same in all channels
+    
+    if len(uutc_map) == 1:
+        uutc_map = uutc_map*len(channel_map)
+        
+    if len(uutc_map) != len(uutc_map):
+        print('Length of sample map is not equivalet to the length of channel map')
+        return
+   
+    for channel,uutc_ss in zip(channel_map,uutc_map):
+        channel_path = session_path+'/'+channel+'.timd'
+        
+        data = pymef3_file.read_mef_ts_data(channel_path,password, uutc_ss[0], uutc_ss[1], True)
+        data_list.append(data)
+        
+    return data_list 
 
-def read_ts_channel_basic_info(mef3_session_path,password):
+def change_channel_name(session_path,orig_channel_name,new_channel_name):
+    """
+    Changes name of channel
+
+    Parameters:
+    -----------
+    session_path - path to mef session\n
+    orig_channel_name - original channel name\n
+    new_channel_name - new channel_name\n
+
+    Returns:
+    --------
+    0 - on success\n
+    """
+
+    # Identify the channel folder
+    channel_folders = os.listdir(session_path)
+    
+    channel_folder = [x for x in channel_folders if x[:x.index('.')] == orig_channel_name][0]
+    channel_path = session_path +'/'+ channel_folder+'/'
+    
+    # Run through segments
+    segment_folders = [x for x in os.listdir(channel_path) if 'segd' in x]
+    channel_files = [x for x in os.listdir(channel_path) if 'segd' not in x]
+    for segment_folder in segment_folders:
+        segment_path = channel_path + segment_folder+'/'
+        # Run through segment_files
+        segment_files = os.listdir(segment_path)
+        for segment_file in segment_files:
+            file_path = segment_path + segment_file
+            new_file_name = new_channel_name + segment_file[segment_file.rindex('-'):] # The right part of the file is fixed
+            new_file_path = segment_path+new_file_name
+            os.rename(file_path,new_file_path)
+    
+        # Rename the segment folder
+        new_segment_folder = new_channel_name + segment_folder[segment_folder.rindex('-'):]
+        new_segment_path = channel_path + new_segment_folder
+        os.rename(segment_path,new_segment_path)
+    
+    # Run through channel files
+    for channel_file in channel_files:
+        file_path = channel_path + channel_file
+        new_file_name = new_channel_name + channel_file[channel_file.rindex('.'):] # The right part of the file is fixed
+        new_file_path = channel_path+new_file_name
+        os.rename(file_path,new_file_path)
+    
+    # Rename the channel folder
+    new_channel_folder = new_channel_name + channel_folder[channel_folder.rindex('.'):]
+    new_channel_path = session_path +'/'+ new_channel_folder
+    os.rename(channel_path,new_channel_path)
+
+    return 0
+
+def read_ts_channel_basic_info(session_path,password):
     """
     Reads session time series channel names
     
     Parameters:
     -----------
-    mef3_session_path - path to mef3 session\n
+    session_path - path to mef3 session\n
     password - mef3 data password\n
     
     Returns:
@@ -320,7 +319,7 @@ def read_ts_channel_basic_info(mef3_session_path,password):
     channel_list\n
     """
     
-    session_ts_metadata_dict = pymef3_file.read_mef_session_metadata(mef3_session_path,password)
+    session_ts_metadata_dict = pymef3_file.read_mef_session_metadata(session_path,password)
     
     channel_list = list(session_ts_metadata_dict['time_series_channels'].keys())
     channel_list.sort()
