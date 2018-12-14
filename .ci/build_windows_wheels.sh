7d81188334396f77d27a1b20de17bc1605987428
Jan Cimbalnik <jan.cimbalnik@fnusa.cz>
2018-12-15 00:42:06 +0100

    switch to sh and choco out of script

--- .ci/build_windows_wheels.sh
+++ .ci/build_windows_wheels.sh
@@ -3,10 +3,6 @@
 set -e
 
 for PYVER in "3.6.3"; do
-  echo "befor python install"
-  /c/ProgramData/chocolatey/bin/choco --version
-  /c/ProgramData/chocolatey/bin/choco install python3 --version 3.6.3
-  echo "after python install"
   python -m pip install numpy wheel
   python setup.py test
   python setup.py bdist_wheel
