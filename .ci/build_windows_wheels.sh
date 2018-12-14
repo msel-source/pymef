3df3fba021b381bed1fe3579d284954a0af62f57
Jan Cimbalnik <jan.cimbalnik@fnusa.cz>
2018-12-15 00:03:48 +0100

    trying without version

--- .ci/build_windows_wheels.sh
+++ .ci/build_windows_wheels.sh
@@ -4,9 +4,7 @@ set -e
 
 for PYVER in "3.6.3"; do
   echo "befor python install"
-  which choco
-  man choco
-  choco install python --version 3.6.3
+  choco install python
   echo "after python install"
   python -m pip install numpy wheel
   python setup.py test
