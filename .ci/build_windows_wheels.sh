a04661d21e068c6f7edc2d6b3c6c545142c25a38
Jan Cimbalnik <jan.cimbalnik@fnusa.cz>
2018-12-15 00:30:22 +0100

    trying absolute path

--- .ci/build_windows_wheels.sh
+++ .ci/build_windows_wheels.sh
@@ -4,8 +4,8 @@ set -e
 
 for PYVER in "3.6.3"; do
   echo "befor python install"
-  choco --version
-  choco install python3 --version 3.6.3
+  /c/ProgramData/chocolatey/bin/choco --version
+  /c/ProgramData/chocolatey/bin/choco install python3 --version 3.6.3
   echo "after python install"
   python -m pip install numpy wheel
   python setup.py test
