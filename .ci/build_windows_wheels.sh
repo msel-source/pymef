7486ad991c812b7b55dcc14cfc95b37c2f269cb5
Jan Cimbalnik <jan.cimbalnik@fnusa.cz>
2018-12-14 23:42:10 +0100

    verbose script for debug

--- .ci/build_windows_wheels.sh
+++ .ci/build_windows_wheels.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env bash
-
+#!/usr/bin/env sh
+echo "before e"
 set -e
-
+echo "before loop"
 for PYVER in "3.6.3"; do
+  echo "befor python install"
   choco install python --version ${PYVER}
+  echo "after python install"
   python -m pip install numpy wheel
   python setup.py test
   python setup.py bdist_wheel
