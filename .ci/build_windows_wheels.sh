1fec73365f75c0eaeaf852555ad6e97cbe3131a9
Jan Cimbalnik <jan.cimbalnik@fnusa.cz>
2018-12-15 00:21:47 +0100

    another go on python with chocolatey

--- .ci/build_windows_wheels.sh
+++ .ci/build_windows_wheels.sh
@@ -4,7 +4,8 @@ set -e
 
 for PYVER in "3.6.3"; do
   echo "befor python install"
-  choco install python
+  choco --version
+  choco install python3 --version 3.6.3
   echo "after python install"
   python -m pip install numpy wheel
   python setup.py test
