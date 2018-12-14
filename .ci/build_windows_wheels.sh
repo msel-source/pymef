b8727baf8b31790573454f26ceaf726b33ed5d85
Jan Cimbalnik <jan.cimbalnik@fnusa.cz>
2018-12-15 00:52:12 +0100

    no secrets

--- .ci/build_windows_wheels.sh
+++ .ci/build_windows_wheels.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e
 
 for PYVER in "3.6.3"; do
+  choco install python3 --version ${PYVER}
   python -m pip install numpy wheel
   python setup.py test
   python setup.py bdist_wheel
