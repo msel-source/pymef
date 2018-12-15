1885b009bec72a95255a0f68184442fa7f4660fd
Jan Cimbalnik <jan.cimbalnik@gmail.com>
2018-12-15 01:41:53 +0100

    export path

--- .ci/build_windows_wheels.sh
+++ .ci/build_windows_wheels.sh
@@ -4,7 +4,7 @@ set -e
 
 for PYVER in "3.6.3"; do
   choco install python3 --version ${PYVER}
-  refreshenv
+  export PATH="/c/Python36:/c/Python36/Scripts:$PATH"
   python -m pip install numpy wheel
   python setup.py test
   python setup.py bdist_wheel
