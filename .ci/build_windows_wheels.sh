6ce65f65a0515f415ce911a24c5235e342195a5e
Jan Cimbalnik <jan.cimbalnik@gmail.com>
2018-12-15 01:35:53 +0100

    refreshenv

--- .ci/build_windows_wheels.sh
+++ .ci/build_windows_wheels.sh
@@ -4,6 +4,7 @@ set -e
 
 for PYVER in "3.6.3"; do
   choco install python3 --version ${PYVER}
+  refreshenv
   python -m pip install numpy wheel
   python setup.py test
   python setup.py bdist_wheel
