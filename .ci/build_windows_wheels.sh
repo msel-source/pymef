6e63dbde8b9210175ada7690093da84a5ee7c9ac
Jan Cimbalnik <jan.cimbalnik@fnusa.cz>
2019-08-08 13:42:51 +0200

    Further fix for windows build

--- .ci/build_windows_wheels.sh
+++ .ci/build_windows_wheels.sh
@@ -6,7 +6,7 @@ declare -a ver_arr=("3.6.3" "3.7.4")
 
 for PYVER in "${ver_arr[@]}"; do
   choco install python3 --version ${PYVER}
-  if [ $PYVER == '3.6.3']; then
+  if [ $PYVER == "3.6.3" ]; then
     export PATH="/c/Python36:/c/Python36/Scripts:$PATH"
   else
     export PATH="/c/Python37:/c/Python37/Scripts:$PATH"
