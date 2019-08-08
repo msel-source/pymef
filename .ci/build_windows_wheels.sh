14b97d6ce260d2b9439e14158cc11b387b2e2123
Jan Cimbalnik <jan.cimbalnik@fnusa.cz>
2019-08-08 13:07:30 +0200

    Fix failing windows build

--- .ci/build_windows_wheels.sh
+++ .ci/build_windows_wheels.sh
@@ -4,10 +4,11 @@ set -e
 
 for PYVER in "3.6.3" "3.7.4"; do
   choco install python3 --version ${PYVER}
-  if [ $PYVER == '3.6.3']
+  if [ $PYVER == '3.6.3']; then
     export PATH="/c/Python36:/c/Python36/Scripts:$PATH"
   else
     export PATH="/c/Python37:/c/Python37/Scripts:$PATH"
+  fi
   python -m pip install numpy wheel
   python setup.py test
   python setup.py bdist_wheel
