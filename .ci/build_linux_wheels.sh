2e954fdec512533d2dedfb4897c38ae9c9c75f0d
Jan Cimbalnik <jan.cimbalnik@fnusa.cz>
2020-06-07 23:56:37 +0200

    Fixes for failing Travis CI builds

--- .ci/build_linux_wheels.sh
+++ .ci/build_linux_wheels.sh
@@ -5,7 +5,7 @@ set -e
 for PYVER in "cp36-cp36m" "cp37-cp37m" "cp38-cp38m"; do
   PYBIN="/opt/python/${PYVER}/bin"
   "${PYBIN}/pip" install numpy
-  "${PYBIN}/pip" install -I wheel==0.30.0 #due to bug in wheel (https://github.com/pypa/wheel/issues/235)
+  "${PYBIN}/pip" install wheel
   "${PYBIN}/python" setup.py test
   "${PYBIN}/python" setup.py bdist_wheel
 done
--- .ci/build_windows_wheels.sh
+++ .ci/build_windows_wheels.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-declare -a ver_arr=("3.6.3" "3.7.4" "3.8.3")
+declare -a ver_arr=("3.6.3" "3.7.4" "3.8")
 
 for PYVER in "${ver_arr[@]}"; do
   choco install python3 --version ${PYVER}
@@ -10,7 +10,7 @@ for PYVER in "${ver_arr[@]}"; do
     export PATH="/c/Python36:/c/Python36/Scripts:$PATH"
   elif [ $PYVER == "3.7.4" ]; then
     export PATH="/c/Python37:/c/Python37/Scripts:$PATH"
-  elif [ $PYVER == "3.8.3" ]; then
+  elif [ $PYVER == "3.8" ]; then
   	export PATH="/c/Python38:/c/Python38/Scripts:$PATH"
   fi
   python -m pip install numpy wheel
